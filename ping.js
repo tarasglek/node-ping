81baafe10360843c399f950fad36a86adc005d82
Taras Glek <taras@glek.net>
2020-11-26 20:46:50 -0800

    fix race condition

--- ping.js
+++ ping.js
@@ -1,7 +1,7 @@
 /*
 docker run --name=ping --rm  -v `pwd`/:/app node:alpine node app/ping.js
 */
-const http = require('http')  
+const http = require('http')
 const url = require('url')
 const child_process = require('child_process');
 const port = 3000
@@ -14,12 +14,21 @@ function maybeErr(error, response, target) {
     }
     return ret
 }
-const requestHandler = (request, response) => {  
+
+let outstandingPings = {}
+let iteration = 0
+const requestHandler = (request, response) => {
     var queryData = url.parse(request.url, true).query;
     var success = 0;
     ret = ""
     var seconds = null
-    child_process.exec(`ping -q -w5 -c1 "${queryData.target}"`, (err, stdout, stderr) => {
+    var target = queryData.target
+    if (target in outstandingPings) {
+        outstandingPings[target].kill(9)
+        console.error("killing outstanding ping for " + target)
+        delete outstandingPings[target]
+    }
+    outstandingPings[target] = child_process.exec(`ping -q -w5 -c1 "${target}"`, (err, stdout, stderr) => {
         if (err) {
             ret += "# "+ err.toString().split("\n").join("\n# ") + "\n"
         } else {
@@ -32,17 +41,18 @@ const requestHandler = (request, response) => {
                 console.error(`${queryData.target} ${ping}`)
             }
 
-            ret += `# TYPE probe_duration_seconds gauge\nprobe_duration_seconds ${seconds}\n`
+            ret += `# TYPE probe_duration_seconds gauge\nprobe_duration_seconds{instance="${target}"} ${seconds}\n`
         }
-        ret += `# TYPE probe_success gauge\nprobe_success ${1*!err}\n`
+        ret += `# ${iteration} TYPE probe_success gauge\nprobe_success{instance="${target}"} ${1*!err}\n`
         response.writeHead(200, {'Content-Type': 'text/plain'});
         response.end(ret)
+        firstCallback = false
     })
 }
 
 const server = http.createServer(requestHandler)
 
-server.listen(port, (err) => {  
+server.listen(port, (err) => {
   if (err) {
     return console.log('something bad happened', err)
   }
